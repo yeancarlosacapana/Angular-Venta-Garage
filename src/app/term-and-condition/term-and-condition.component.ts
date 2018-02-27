b3448a6880dcdb8ed5562708705a239af472d89e
davidchoquerojas <choquerojasnoedavid@gmail.com>
2018-02-27 17:53:59 -0600

    change ls

---
+++ src/app/term-and-condition/term-and-condition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-term-and-condition',
+  templateUrl: './term-and-condition.component.html',
+  styleUrls: ['./term-and-condition.component.sass']
+})
+export class TermAndConditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
